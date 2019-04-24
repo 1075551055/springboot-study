5758b7dc6f7abcccdd65791cffe800f24eba8250
liyanmin <13824774439@163.com>
2019-04-24 16:15:46 +0800

    easy hello world(simple unit test)

--- src/main/java/com/water/springbootstudy/web/HelloController.java
+++ src/main/java/com/water/springbootstudy/web/HelloController.java
@@ -1,4 +1,16 @@
 package com.water.springbootstudy.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+//    control + return 相当于window上的alt+insert
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+
 }
