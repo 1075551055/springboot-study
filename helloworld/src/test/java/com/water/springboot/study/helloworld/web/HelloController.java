2844143dcd71a30a89fde493d0d45340ffad431c
liyanmin <13824774439@163.com>
2019-04-24 16:44:12 +0800

    change hello world example to module

--- helloworld/src/test/java/com/water/springboot/study/helloworld/web/HelloController.java
+++ helloworld/src/test/java/com/water/springboot/study/helloworld/web/HelloController.java
@@ -1,4 +1,4 @@
-package com.water.springbootstudy.web;
+package com.water.springboot.study.helloworld.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
