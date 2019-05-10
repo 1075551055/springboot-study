693aa2cfb693430bb4ab039b3766c5e27c4b6323
liyanmin <13824774439@163.com>
2019-05-10 16:40:40 +0800

    添加不同环境（开发环境和生产环境）的属性配置文件

--- springboot_in_action_sample/src/main/java/com/water/springboot/study/readinglist/ReadingListController.java
+++ springboot_in_action_sample/src/main/java/com/water/springboot/study/readinglist/ReadingListController.java
@@ -1,7 +1,7 @@
 package com.water.springboot.study.readinglist;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +11,17 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/")
+//表示该controller的属性是通过配置属性（application.properties）进行注入的，前缀是amazon
+@ConfigurationProperties(prefix = "amazon")
 public class ReadingListController {
-    private ReadlingListRepository readlingListRepository;
+    private String associateId;
 
+    //ConfigurationProperties注解是通过set方式进行属性的注入
+    public void setAssociateId(String associateId) {
+        this.associateId = associateId;
+    }
+
+    private ReadlingListRepository readlingListRepository;
     public ReadingListController(ReadlingListRepository readlingListRepository) {
         this.readlingListRepository = readlingListRepository;
     }
@@ -23,6 +31,8 @@ public class ReadingListController {
         List<Book> books = readlingListRepository.findByReader(reader);
         if(books != null){
             model.addAttribute("books", books);
+            model.addAttribute("reader", reader);
+            model.addAttribute("amazonID", associateId);
         }
         return "readingList";
     }
