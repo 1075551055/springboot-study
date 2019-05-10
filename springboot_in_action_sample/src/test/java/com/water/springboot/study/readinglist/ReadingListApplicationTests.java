07f89e1b98657a7cd91ba4aef8ec8d73379b1fe0
liyanmin <13824774439@163.com>
2019-05-10 17:50:07 +0800

    添加spring boot单元测试资源

--- springboot_in_action_sample/src/test/java/com/water/springboot/study/readinglist/ReadingListApplicationTests.java
+++ springboot_in_action_sample/src/test/java/com/water/springboot/study/readinglist/ReadingListApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+//integration test annotation, if unit test pls refer to(@WebMvcTest) "https://www.baeldung.com/spring-boot-testing"
 @SpringBootTest
 public class ReadingListApplicationTests {
 
